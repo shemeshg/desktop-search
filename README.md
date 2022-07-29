426d1aef705f4d13dc6170163460a3e5ea61eace
osx user <gili.shemesh@gmail.com>
2022-07-29 19:56:31 +0300

    deploy

---
+++ README.md
@@ -0,0 +1,1 @@
+# v1.0.1 desktop-search  http://shemeshg.github.io/desktop-search
---
+++ precache-manifest.6235244b738ac1103bc8583c81728326.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b135c866f36df15f236",
+    "url": "/desktop-search/css/app.6d0525b9.css"
+  },
+  {
+    "revision": "265f655e179f8d0d0055",
+    "url": "/desktop-search/css/chunk-vendors.5cdf5eda.css"
+  },
+  {
+    "revision": "8a8551604328933f4fe0b2261547601d",
+    "url": "/desktop-search/index.html"
+  },
+  {
+    "revision": "1b135c866f36df15f236",
+    "url": "/desktop-search/js/app.41b6e3cd.js"
+  },
+  {
+    "revision": "5b407072775a901a50f7",
+    "url": "/desktop-search/js/chunk-2d0e2138.8dc7c25a.js"
+  },
+  {
+    "revision": "57299a712dba19b6fa52",
+    "url": "/desktop-search/js/chunk-2d22d746.7b6e735c.js"
+  },
+  {
+    "revision": "e363a556001879b04cc0",
+    "url": "/desktop-search/js/chunk-3148c4f8.42866b5b.js"
+  },
+  {
+    "revision": "60a681418e1fded2507e",
+    "url": "/desktop-search/js/chunk-625e946b.d33c94cf.js"
+  },
+  {
+    "revision": "4ffbff7bb2c54f463ec9",
+    "url": "/desktop-search/js/chunk-eaf0c03c.891a37bf.js"
+  },
+  {
+    "revision": "265f655e179f8d0d0055",
+    "url": "/desktop-search/js/chunk-vendors.a521c3db.js"
+  },
+  {
+    "revision": "f46cf60c85491680753ccb19c0670ec2",
+    "url": "/desktop-search/manifest.json"
+  },
+  {
+    "revision": "3dc6357e767058fae8172b8a9f41a114",
+    "url": "/desktop-search/opensearch.xml"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/desktop-search/robots.txt"
+  }
+]);
